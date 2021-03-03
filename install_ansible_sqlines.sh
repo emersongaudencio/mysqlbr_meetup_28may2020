871ee8061e0112ed2666d18e941e3a6c487e7059
Emerson Gaudencio <emerson.fender@gmail.com>
2021-03-02 23:23:18 -0300

    updating sqlines installation process

--- install_ansible_sqlines.sh
+++ install_ansible_sqlines.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
-verify_wget=`rpm -qa | grep wget`
-if [[ $verify_wget == "wget"* ]]
-then
-echo "$verify_wget is installed!"
+#### install python3 #####
+verify_python=`rpm -qa | grep python3-3`
+if [[ "${verify_python}" == "python3-3"* ]] ; then
+   echo "$verify_python is installed!"
 else
-   sudo yum install wget -y
+   yum -y install python3
 fi
 
-verify_python=`rpm -qa | grep python-2.7`
-if [[ $verify_python == "python-2.7"* ]]
-then
-echo "$verify_python is installed!"
+#### install git #####
+verify_git=`rpm -qa | grep git-1`
+if [[ "${verify_git}" == "git"* ]] ; then
+   echo "$verify_git is installed!"
 else
-   sudo yum install python -y
+   yum install git -y
 fi
 
-verify_git=`rpm -qa | grep git`
-if [[ $verify_git == "git"* ]]
-then
-echo "$verify_git is installed!"
-else
-   sudo yum install git -y
-fi
-
+#### install pip #####
 verify_pip=`pip -V`
-if [[ $verify_pip == "pip"* ]]
-then
-echo "$verify_pip is installed!"
+if [[ "${verify_pip}" == "pip"* ]] ; then
+   echo "$verify_pip is installed!"
+   echo "Pip-Path: $(which pip)"
 else
-   cd /opt
-   sudo rm -rf get-pip.py
-   sudo wget https://bootstrap.pypa.io/get-pip.py
-   sudo python get-pip.py
+   curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+   source ~/.bashrc
+   pip -V
+   echo "Pip-Path: $(which pip)"
 fi
 
-### install ansible #####
-sudo pip install ansible --upgrade
-ansible --version
+#### install ansible #####
+verify_ansible=`ansible --version`
+if [[ "${verify_ansible}" == "ansible"* ]] ; then
+  echo "$verify_ansible is installed!"
+  echo "Ansible-Path: $(which ansible)"
+else
+  python3 -m pip install ansible
+  source ~/.bashrc
+  ansible --version
+  echo "Ansible-Path: $(which ansible)"
+fi
 
-cd /opt
-git clone https://github.com/emersongaudencio/ansible-oracle-to-mariadb-migration.git
-cd ansible-oracle-to-mariadb-migration
-sudo sh run_sqlines.sh sqlines-local mysqlines
+### mariadb install ###
+if [[ -d "/opt/ansible-oracle-to-mariadb-migration" ]] ; then
+  echo "Directory /opt/ansible-oracle-to-mariadb-migration exists."
+else
+  cd /opt
+  git clone https://github.com/emersongaudencio/ansible-oracle-to-mariadb-migration.git
+  cd ansible-oracle-to-mariadb-migration
+  sed -ie 's/ansible/\/usr\/local\/bin\/ansible/g' run_sqlines.sh
+  bash run_sqlines.sh sqlines-local mysqlines
+fi
